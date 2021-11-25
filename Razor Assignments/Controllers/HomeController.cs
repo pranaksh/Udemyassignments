cac11a502d653f630436a62b6f446b499204ef63
Daffolap-824 <Daffolap-824@DESKTOP-0FLI380>
2021-11-25 17:03:11 +0530

    Add project files.

---
+++ Razor Assignments/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Razor_Assignments.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(int amount=0)
+        {
+            ViewBag.amount = amount;
+            ViewBag.denom = new List<int>() { 1000, 500, 100, 10, 1 };
+            return View();
+        }
+    }
+}
---
+++ Razor Assignments/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Razor_Assignments.MvcApplication" Language="C#" %>
